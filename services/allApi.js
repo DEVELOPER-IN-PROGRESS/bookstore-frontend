10ea0ebedcf23039e134941c07c92d0f87e923c9
iceweasel <johnsjosephk@pm.me>
2025-07-05 12:58:12 +0530

    all api's file refactored

--- services/allApi.js
+++ services/allApi.js
@@ -1,101 +1,98 @@
-import  { serverUrl } from './serverurl';
 import { commonApi } from  './commonApi';
 
 export const registerApi = async(reqBody) => {
- return await commonApi('POST' , `${serverUrl}/register`, reqBody) ;
+ return await commonApi('POST' , `/register`, reqBody) ;
 };
 
 export const loginApi = async(reqBody) => {
-    return await commonApi('POST', `${serverUrl}/login`, reqBody)
+    return await commonApi('POST', `/login`, reqBody)
 }
 
 //google login API
 export const googleLoginApi = async(reqBody) => {
-    return await commonApi('POST', `${serverUrl}/google-login`, reqBody)
+    return await commonApi('POST', `/google-login`, reqBody)
 }
 
 //get home books
 export const homeBookApi = async() =>{
-    return await commonApi('GET',`${serverUrl}/all-home-book`)
+    return await commonApi('GET',`/all-home-book`)
 }
 
 // -------------------------------- users  api ------------------------------------
 
 // upload a  book
 export const uploadBookApi = async(reqHeader,reqBody) => {
-    return await commonApi('POST',`${serverUrl}/add-books`,reqBody,reqHeader)
+    return await commonApi('POST',`/add-books`,reqBody,reqHeader)
 }
 
 export const getAllBookApi = async(searchKey, reqHeader) =>{
     // query Paremeters baseurl?key=value
-    return await commonApi('GET',`${serverUrl}/all-books?search=${searchKey}`, '' ,reqHeader)
+    return await commonApi('GET',`/all-books?search=${searchKey}`, '' ,reqHeader)
 }
 
 export const ViewSingleBookApi = async(id) => {
-    return await commonApi('GET',`${serverUrl}/view-book/${id}`) ;
+    return await commonApi('GET',`/view-book/${id}`) ;
 }
 
 export const getAllAdminBookApi = async(reqHeader) => {
-    return await commonApi('GET',`${serverUrl}/admin-books`,'',reqHeader)
+    return await commonApi('GET',`/admin-books`,'',reqHeader)
 }
 
 //api to approve a book from an admin
 
 export const approveBookApi = async(reqBody,reqHeader) => {
-    return await commonApi('PUT',`${serverUrl}/approve-book`,reqBody, reqHeader)
+    return await commonApi('PUT',`/approve-book`,reqBody, reqHeader)
 }
 
 export const getAllUsersApi = async(reqHeader) => {
-    return await commonApi('GET', `${serverUrl}/all-users`,'', reqHeader)
-
+    return await commonApi('GET', `/all-users`,'', reqHeader)
 }
 
 //api to add the jobs
 export const addJobApi = async(reqBody) =>{
-    return await commonApi("POST",`${serverUrl}/add-job`,reqBody)
+    return await commonApi("POST",`/add-job`,reqBody)
 }
 
 //api to get all the jobs list from backend
 export const getAllJobsApi = async(search) =>{
-    return await commonApi('GET',`${serverUrl}/all-jobs?search=${search}`)
+    return await commonApi('GET',`/all-jobs?search=${search}`)
 }
 
 // api to delete a job by admin
 export const deleteAJobApi = async(id) => {
-    return await commonApi('DELETE',`${serverUrl}/delete-job/${id}`)
+    return await commonApi('DELETE',`/delete-job/${id}`)
 }
 
 //api to apply for a job
 export const addApplicationApi  = async (reqHeader,reqBody) => {
-    return await commonApi('POST',`${serverUrl}/apply-job`,reqBody,reqHeader);
+    return await commonApi('POST',`/apply-job`,reqBody,reqHeader);
 }
 
 //api to get all the applications from the users
 
 export const getAllApplicationsApi = async() => {
-    return await commonApi('GET',`${serverUrl}/all-application`)
+    return await commonApi('GET',`/all-application`)
 }
 
 //api to update the profile in the backend
-
 export const updateProfileApi = async(reqBody , reqHeader , user ) => {
     if(user)
-        return await commonApi('PUT',`${serverUrl}/user-profile-update`,reqBody,reqHeader)
-    return await commonApi('PUT', `${serverUrl}/admin-profile-update`, reqBody, reqHeader )
+        return await commonApi('PUT',`/user-profile-update`,reqBody,reqHeader)
+    return await commonApi('PUT', `/admin-profile-update`, reqBody, reqHeader )
 }
 
 export const getAllUserBroughtBookApi = async(reqHeader) => {
-    return await commonApi('GET', `${serverUrl}/user-brought-books`, '', reqHeader)
+    return await commonApi('GET', `/user-brought-books`, '', reqHeader)
 }
 
 export const getAllUserBooksApi  = async(reqHeader) => {
-    return await commonApi ('GET',`${serverUrl}/user-books`, '', reqHeader)
+    return await commonApi ('GET',`/user-books`, '', reqHeader)
 }
 
 export const deleteUserBookApi = async(id ) => {
-    return await commonApi ('DELETE',`${serverUrl}/delete-user-books/${id}`,'','')
+    return await commonApi ('DELETE',`/delete-user-books/${id}`,'','')
 }
 
 export const makePaymentApi = async(reqHeader, reqBody) => {
-    return await commonApi('PUT',`${serverUrl}/make-payment`,reqBody,reqHeader)
+    return await commonApi('PUT',`/make-payment`,reqBody,reqHeader)
 }
--- services/commonApi.js
+++ services/commonApi.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
+import { serverUrl } from './serverurl'
 
-export const commonApi = async  (httpMethod, url , reqBody , reqHeader ) => {
+export const commonApi = async  (httpMethod, endpoint , reqBody , reqHeader ) => {
 	const reqConfig = {
 		method: httpMethod ,
-		url ,
+		url: `${serverUrl}${endpoint}` ,
 		data: reqBody ,
 		headers: reqHeader
 	}
-
 	return  await axios(reqConfig)
 		.then( (res) => { return res })
 		.catch( (err) =>  { return err })
